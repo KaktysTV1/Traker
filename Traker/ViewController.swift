2fa69ed57569184da7bd9cfa5c978b18644c16b2
KaktysTV1 <wotmaster187@iclod.com>
2023-11-24 10:55:19 +0300

    Initial Commit

---
+++ Traker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traker
+//
+//  Created by Андрей Чупрыненко on 24.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
